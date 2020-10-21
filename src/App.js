c876e228d64ce385a6af036a9d6e27dc468db177
Bhushan Wagh <bhushanwagh6@gmail.com>
2020-10-22 00:02:14 +0530

    infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,89 +1,88 @@
 import React from 'react';
+import {useState, useEffect} from 'react'
 import './App.css';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import Card from './component/Card';
-import Header from './component/Header'
-// import Pagination from './component/Pagination';
+import Card from '../src/Components/Card'
+import Header from "../src/Components/Header"
 
-let pokeStartId = 1;
+let pokemon = [];
+let inLim = 50;
+let inOff = 0;
 
-const App = () => {
 
+function App() {
   const [data, setData] = useState([]);
-  let poke = [];
-  const [startId, setStartId] = useState(pokeStartId)
-  const [isLoading, setIsLoading] = useState(false)
+  const [lim, setLim] = useState(inLim);
+  const [off, setOff] = useState(inOff);
+  const [loading, setLoading] = useState(true)
 
-  const nextPage = () => {
-    if (pokeStartId <= 751) {
-      pokeStartId+=50
-      setStartId(pokeStartId);
-      setIsLoading(false)
-    } else{
-      setStartId(1)
-      setIsLoading(false)
+
+useEffect(() => {
+
+  async function getData(limit, off) {
+    let resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${off}`)
+    let data = await resp.json()
+    let poke = data.results
+    // console.log(poke)
+    for (let index = 0; index < poke.length; index++) {
+        const element = poke[index];
+        let respSingle = await fetch(element.url)
+        let singlePoke = await respSingle.json()
+        pokemon.push(singlePoke)
     }
-  }
+    setData(pokemon)
+    setLoading(false)
+}
 
-  const previousPage = () => {
-    if(pokeStartId >= 50){
-      pokeStartId-=50
-      setStartId(pokeStartId)
-      setIsLoading(false)
+window.addEventListener('scroll', function () {
+
+  let scrolling = window.innerHeight + window.scrollY;
+  let scrollable = document.body.offsetHeight * 0.9;
+
+  if (scrolling >= scrollable){
+    if(inLim === 50 && inOff === 0){
+      inLim = 10;
+    inOff += 50
+    setOff(inOff)
+    setLoading(true)
+    console.log(loading)
+    getData(inLim, inOff)
+    console.log(loading)
+    // console.log(inLim, inOff)
     } else{
-      setStartId(751)
-      setIsLoading(false)
+      inLim = 10;
+      inOff+=10
+      // console.log(inLim, inOff)
+      setOff(inOff)
+      setLoading(true)
+      console.log(loading)
+      getData(inLim, inOff)
+      console.log(loading)
     }
-  }
-
   
-
-  useEffect(() => {
-
-    nextPage()
-    previousPage()
-
-    const getData = async() => {
-      for (let i = startId; i <= (startId+49); i++) {
-        let result = await axios(` https://pokeapi.co/api/v2/pokemon/${i}`)
-        poke.push(result.data)
-        
-      }
-      setData(poke)
-      setIsLoading(true)
-      console.log(poke)
-    }
-    getData()
-  }, [startId])
+  }
+  
+})
+    getData(inLim, inOff)
+  }, [lim])
 
 
 
-  if (isLoading) {
-    return (<div className="container">
-        <Header/>
-        {data.map(item => (
-          <Card key= {item.id}  name= {item.name} imgUrl={item.sprites.other["official-artwork"].front_default} rank={item.id} stats={item.stats} pokeType={item.types[0].type.name}/>
-        ))}
-          <footer>
-              <button className="previous" onClick = {previousPage}>Previous</button>
-              <button className="next" onClick={nextPage}>Next</button>
-          </footer>
-      </div>
-    )} else{
-      return(
-        <div className="container">
-          <Header/>
-          <div className="loadingContainer">
-            <div className="loader"></div>
-          </div>
-          <footer>
-              <button className="previous" onClick = {previousPage}>Previous</button>
-              <button className="next" onClick={nextPage}>Next</button>
-          </footer>
-        </div>
-      )
-    }
+  return (
+    <div className="container" >
+      
+         <Header/>
+         {data.map(item => (
+           <Card 
+            key = {item.id}
+           name= {item.name} 
+           imgUrl={item.sprites.other["official-artwork"].front_default} 
+           rank={item.id} 
+           stats={item.stats} 
+           pokeType={item.types[0].type.name}
+           />
+         ))}
+    </div>
+  );
 }
 
 export default App;
