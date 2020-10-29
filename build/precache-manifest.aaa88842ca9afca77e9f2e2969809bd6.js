ca36a760841bfefcd0db9b9c0a169263a5f850be
Bhushan Wagh <bhushanwagh6@gmail.com>
2020-10-29 19:25:51 +0530

    Build

---
+++ build/precache-manifest.aaa88842ca9afca77e9f2e2969809bd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d36fc88dd208e251d1224c8f3110a7",
+    "url": "/PokemonApp/index.html"
+  },
+  {
+    "revision": "f2472f1024890ea9b036",
+    "url": "/PokemonApp/static/css/main.6bf35787.chunk.css"
+  },
+  {
+    "revision": "039530e632b1d8144ead",
+    "url": "/PokemonApp/static/js/2.987e24ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/PokemonApp/static/js/2.987e24ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2472f1024890ea9b036",
+    "url": "/PokemonApp/static/js/main.cf4216f0.chunk.js"
+  },
+  {
+    "revision": "5c2209cf929cf0ce41eb",
+    "url": "/PokemonApp/static/js/runtime-main.af2f1bbc.js"
+  }
+]);
